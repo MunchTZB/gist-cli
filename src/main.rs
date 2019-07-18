e2b7c027513c2acc31196677e045f1342bde6bdf
汤振邦 <tangzhenbang@bytedance.com>
2019-07-18 16:12:45 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+extern crate clap;
+extern crate reqwest;
+use clap::{Arg, App};
+
 fn main() {
-   let mut s = String::from("hello");
-   s.push_str(", world");
-   println!("{}", s);
+    let matches = App::new("gist-cli")
+                        .version("0.0.1")
+                        .author("MunchTZB <dev.tzb@gmail.com>")
+                        .about("a github gist cli tool.")
+                        .arg(Arg::with_name("INPUT_FILE")
+                             .help("Set the target file")
+                             .required(true)
+                             .index(1))
+                        .get_matches();
+
+    let input_file = matches.value_of("INPUT_FILE").unwrap();
+    let input_file = std::path::PathBuf::from(input_file);
+    let content = std::fs::read_to_string(input_file).unwrap();
+    let resp = reqwest::get("http://www.baidu.com").unwrap().text().unwrap();
+
+    println!("{}", resp);
 }
