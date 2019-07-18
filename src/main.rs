71ad3392ef484c18104174ed70dd1a561235affd
汤振邦 <tangzhenbang@bytedance.com>
2019-07-18 21:48:01 +0800

    updaet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate clap;
 extern crate reqwest;
+extern crate dirs;
 use clap::{Arg, App};
+use std::collections::HashMap;
+use reqwest::Client;
 
 fn main() {
     let matches = App::new("gist-cli")
@@ -13,10 +16,22 @@ fn main() {
                              .index(1))
                         .get_matches();
 
+    let mut rc_path = std::path::PathBuf::from(dirs::home_dir().unwrap());
+    rc_path.push(".gist-cli");
+
+    let rc = std::fs::read_to_string(rc_path).unwrap();
+    let rc = rc.trim();
+
     let input_file = matches.value_of("INPUT_FILE").unwrap();
     let input_file = std::path::PathBuf::from(input_file);
     let content = std::fs::read_to_string(input_file).unwrap();
-    let resp = reqwest::get("http://www.baidu.com").unwrap().text().unwrap();
 
-    println!("{}", resp);
+
+    let clients = Client::new();
+    let mut resp = clients.get("https://api.github.com/users/codertocat").send().unwrap();
+    for (key, value) in resp.headers().iter() {
+        println!("{:?}: {:?}", key, value);
+    }
+
+    println!("{:#?}", resp);
 }
